cd654b58e647027272b57b39c80d38dd1deb55a1
Ahmed Ghazy <banscio98@gmail.com>
2026-06-29 05:46:58 +0300

    Bump version to 1.3.0

--- src/js/app.js
+++ src/js/app.js
@@ -84,7 +84,7 @@ updateClock();
 setInterval(updateClock, 1000);
 
 /* ── Update system (electron-updater) ── */
-var APP_VERSION = '1.2.2';
+var APP_VERSION = '1.3.0';
 var updateData = null;
 var updateDownloaded = false;
 

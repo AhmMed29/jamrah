de7c2b5c67a9104f296fc252714d2e2f45c76935
Ahmed Ghazy <banscio98@gmail.com>
2026-08-14 16:21:04 +0300

    Feature : Prayer Times

---
+++ Models/PrayerTimeEntry.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+
+namespace Jamrah.Models
+{
+    [Table("PrayerTimes")]
+    public class PrayerTimeEntry
+    {
+        [PrimaryKey]
+        public string DateKey { get; set; } = "";
+
+        public string Fajr    { get; set; } = "";  
+        public string Dhuhr   { get; set; } = "";  
+        public string Asr     { get; set; } = ""; 
+        public string Maghrib { get; set; } = ""; 
+        public string Isha    { get; set; } = ""; 
+
+        public double Latitude  { get; set; }
+        public double Longitude { get; set; }
+        public int    Method    { get; set; }
+    }
+}
